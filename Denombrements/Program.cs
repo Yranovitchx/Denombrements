3945c8d72f75db0ae2ab407ac99c925a5190cdab
Yranovitchx <Yranovitchx@LAPTOP-52F2MMSH>
2022-03-11 08:27:12 +0100

    Ajoutez des fichiers projet.

---
+++ Denombrements/Program.cs
@@ -0,0 +1,85 @@
+﻿/**
+-Ouvrez le sous Visual Studio et publiez-le sur GitHub.
+-Fermez le projet et, dans un nouveau dossier, ouvrez un nouveau projet à partir du clone du premier projet (avec l'adresse de clone récupérée sur GitHub)
+-Ouvrez la solution et visualisez le code.
+-Créez une branche.
+-Analysez le code et, pour chaque sous domaine* présenté dans le rapport d'analyse de la revue de code (si vous ne l'avez pas encore récupéré, vous pouvez le télécharger ici) :
+-Apportez les corrections qui vous paraissent pertinentes.
+-Testez à nouveau le programme pour voir s'il marche correctement.
+-Remplissez le rapport d'analyse de la revue de code.
+-Répétez l'étape 6 jusqu'à ce que vous ayez traité tous les points.
+-Envoyez les modifications vers GitHub (valider tout et pousser).
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denombrements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 1;
+            while (c != 0)
+            {
+                Console.WriteLine("Permutation ...................... 1");
+                Console.WriteLine("Arrangement ...................... 2");
+                Console.WriteLine("Combinaison ...................... 3");
+                Console.WriteLine("Quitter .......................... 0");
+                Console.Write("Choix :                            ");
+                c = int.Parse(Console.ReadLine());
+
+                if (c == 0) { Environment.Exit(0); }
+
+                if (c == 1)
+                {
+                    Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
+                    int n = int.Parse(Console.ReadLine()); // saisir le nombre
+                                                           // calcul de r
+                    long r = 1;
+                    for (int k = 1; k <= n; k++)
+                        r *= k;
+                    Console.WriteLine(n + "! = " + r);
+                }
+                else
+                {
+                    if (c == 2)
+                    {
+                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
+                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
+                        Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
+                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
+                        // calcul de r
+                        long r = 1;
+                        for (int k = (t - n + 1); k <= t; k++)
+                            r *= k;
+                        //Console.WriteLine("résultat = " + (r1 / r2));
+                        Console.WriteLine("A(" + t + "/" + n + ") = " + r);
+                    }
+                    else
+                    {
+                        Console.Write("nombre total d'éléments à gérer = "); // le nombre d'éléments à gérer
+                        int t = int.Parse(Console.ReadLine()); // saisir le nombre
+                        Console.Write("nombre d'éléments dans le sous ensemble = "); // le sous ensemble
+                        int n = int.Parse(Console.ReadLine()); // saisir le nombre
+                        // calcul de r1
+                        long r1 = 1;
+                        for (int k = (t - n + 1); k <= t; k++)
+                            r1 *= k;
+                        // calcul de r2
+                        long r2 = 1;
+                        for (int k = 1; k <= n; k++)
+                            r2 *= k;
+                        // calcul de r3
+                        //Console.WriteLine("résultat = " + (r1 / r2));
+                        Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
